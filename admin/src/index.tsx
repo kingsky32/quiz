f01a88c7405955ee4e237fdbd72e0a546cef6c58
Seung Ju <kingsky32@gmail.com>
2023-11-30 20:07:45 +0900

    fix: Browse Router to MemoryRouter

--- admin/src/index.tsx
+++ admin/src/index.tsx
@@ -4,7 +4,7 @@ import './styles/index.css';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { App as AntdApp } from 'antd';
-import { BrowserRouter } from 'react-router-dom';
+import { MemoryRouter } from 'react-router-dom';
 import Api from './apis/api';
 import App from './app/App';
 
@@ -12,11 +12,11 @@ const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement,
 );
 root.render(
-  <BrowserRouter>
+  <MemoryRouter>
     <AntdApp>
       <Api.Provider>
         <App />
       </Api.Provider>
     </AntdApp>
-  </BrowserRouter>,
+  </MemoryRouter>,
 );
